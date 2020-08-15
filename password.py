7e0707fddd5929ed2ee978f1ba88b636084b4403
Cyrus Tse <kaho0533@yahoo.com.hk>
2020-08-15 20:34:42 +0800

    change to while>0

--- password.py
+++ password.py
@@ -1,12 +1,11 @@
 password = 'a123456'
 i = 3 
-while True:
-	pwd = input('please imput your password')
+while i > 0:
+	pwd = input('please imput your password:')
 	if pwd == password:
 		print('you password is successful')
 		break
 	else:
 		i = i - 1
 		print('password wrong ! you just have' , i , 'chances')
-		if i == 0:
-			break
+		
